0c807b671572ba76879a6f58730fb7faf9d59d1d
zg <3020498526@qq.com>
2023-10-30 20:30:22 +0800

    menu header@1.0.1

--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -12,9 +12,19 @@ export default {
       header-left
     </div>
     <div class="r-content">
+      <!--设置为一行-->
       <el-row>
-        <el-button type="info" plain>我</el-button>
-        <el-button type="warning" plain>退出</el-button>
+        <el-dropdown>
+          <span class="el-dropdown-link">
+            <img src="../../src/assets/user.jpg" alt="" class="user-image">
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>基本信息</el-dropdown-item>
+            <el-dropdown-item disabled>配置信息</el-dropdown-item>
+            <el-dropdown-item divided>退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+        <!--        <el-button type="warning" plain>退出</el-button>-->
       </el-row>
     </div>
   </div>
@@ -29,5 +39,12 @@ export default {
   height: 70px;
   background: #333;
   color: #fff;
+
+  .user-image {
+    padding: 0 20px;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%; // 圆边
+  }
 }
 </style>
